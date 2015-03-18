13284e408bf1b3623a5fd928f9a93719658945fd
Ernesto De los Santos Cordero <ernestodlsc@gmail.com>
2015-03-18 08:07:52 -0400

    Uri added to contracts

--- app/src/main/java/io/bananalabs/dailyrandom/data/DailyRandomContract.java
+++ app/src/main/java/io/bananalabs/dailyrandom/data/DailyRandomContract.java
@@ -1,5 +1,8 @@
 package io.bananalabs.dailyrandom.data;
 
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 /**
@@ -7,7 +10,19 @@ import android.provider.BaseColumns;
  */
 public class DailyRandomContract {
 
+    public static final String CONTENT_AUTHORITY = "io.bananlabs.dailyrandom";
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+    public static final String PATH_CATEGORY = "category";
+    public static final String PATH_ELEMENT = "element";
+
     public static final class CategoryEntry implements BaseColumns {
+
+        public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon().appendPath(PATH_CATEGORY).build();
+        public static final String CONTENT_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE +
+                CONTENT_AUTHORITY + "/" + PATH_CATEGORY;
+        public static final String CONTENT_ITEM_TYPE = ContentResolver.CURSOR_ITEM_BASE_TYPE +
+                CONTENT_AUTHORITY + "/" + PATH_CATEGORY;
+
         public static final String TABLE_NAME = "category";
 
         // String, identify the category
@@ -15,14 +30,24 @@ public class DailyRandomContract {
         // Integer, color assigned to the categoty
         public static final String COLUMN_COLOR = "color";
 
+        public static Uri buildCategoryUri(long id) {
+            return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
+
     }
 
     public static final class ElementEntry implements BaseColumns {
+
+        public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon().appendPath(PATH_ELEMENT).build();
+        public static final String CONTENT_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE +
+                CONTENT_AUTHORITY + "/" + PATH_ELEMENT;
+        public static final String CONTENT_ITEM_TYPE = ContentResolver.CURSOR_ITEM_BASE_TYPE +
+                CONTENT_AUTHORITY + "/" + PATH_ELEMENT;
+
         public static final String TABLE_NAME = "element";
 
         // Column with the foreign key into the category table
         public static final String COLUMN_CAT_KEY = "category_id";
-
         // String, identify the element
         public static final String COLUMN_TITLE = "title";
         // Integer, counts how many times the element has been chosen
@@ -31,5 +56,9 @@ public class DailyRandomContract {
         public static final String COLUMN_COORD_LAT = "coord_lay";
         // Double, longitude for latitude (if apply)
         public static final String COLUMN_COORD_LONG = "coord_long";
+
+        public static Uri buildElementUri(long id) {
+            return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
     }
 }
